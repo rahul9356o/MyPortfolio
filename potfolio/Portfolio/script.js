d891a62962653ad6b95e28330e742782cf99c4f4
Rahul Vijay Nalawade <150572262+rahul9356o@users.noreply.github.com>
2024-08-10 11:32:21 +0530

    Add files via upload

---
+++ potfolio/Portfolio/script.js
@@ -0,0 +1,17 @@
+
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer.", "Web Developer.", "UI Designer.", "Java Developer."],
+  typeSpeed: 70,
+  backSpeed: 40,
+  loop: true
+
+})
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
